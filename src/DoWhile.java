5831c5219f49b2459002c74000bf4c61bda0ed96
Bintangilham10 <bintang.ilham1401@gmail.com>
2021-01-15 07:27:10 +0700

    Do while

---
+++ src/DoWhile.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Bintang.Ilham
+ */
+public class DoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //deklarasi variabel//
+        int bil = 1;
+        
+        //perulangan//
+        do{
+            
+            //pernyataan//
+            System.out.print(bil+"  ");
+            bil++;
+        
+        //rumus batas perulangan//
+        }while (bil<=10);
+    }
+    
+}
